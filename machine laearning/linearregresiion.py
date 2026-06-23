c140b6b8f2a86d4b9b2ce52f521b80e5829dd119
Keshav <saharankeshav747@gmail.com>
2026-06-23 11:00:14 +0530

    23june

--- machine laearning/linearregresiion.py
+++ machine laearning/linearregresiion.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+
 
 
 experience = [1, 2, 3, 4, 5, 6]   
@@ -34,12 +34,16 @@ model = LinearRegression()
 X = df.iloc[:, 0:1]
 y = df.iloc[:, -1]
 
-X_train , y_train , X_test , y_test = train_test_split(X, y , test_size = 0.2 )
+from sklearn.model_selection import train_test_split
+
+X_train , X_test , y_train , y_test = train_test_split(X, y , test_size = 0.2 )
 # print(X_train.shape)
 
 model.fit(X_train,y_train)
 
 y_pred = model.predict(X_test)
 
-print(model.score(X_test,y_test))
+# print(model.score(X_test,y_test))
+
+print(model.predict([[10]]))
 
